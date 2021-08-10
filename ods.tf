113d26b7388024110de5c5124478f9a0d363b7fa
Lyudmil Pelov <lyudmil.x.pelov@oracle.com>
2021-08-10 15:54:36 +0200

    - fix the oci_datascience_notebook_session not able to create due to VCN timeout - prepare the repo for the ORM GIT Support

--- ods.tf
+++ ods.tf
@@ -6,6 +6,12 @@
 #           ODS Project
 #*************************************
 
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [oci_core_subnet.ods-private-subnet]
+  create_duration = "60s"
+}
+
+
 resource "oci_datascience_project" "ods-project" {
   count = var.enable_ods ? 1 : 0
   #Required
@@ -24,15 +30,14 @@ resource "oci_datascience_notebook_session" "ods-notebook-session" {
   compartment_id = var.compartment_ocid
   notebook_session_configuration_details {
     #Required
-
     shape = var.ods_compute_shape
     # subnet_id = local.private_subnet_id
     subnet_id = var.ods_vcn_use_existing ? var.ods_subnet_private_existing : oci_core_subnet.ods-private-subnet[0].id
 
-
     #Optional
     block_storage_size_in_gbs = var.ods_storage_size
   }
+
   project_id = oci_datascience_project.ods-project[0].id
 
   display_name = "${var.ods_notebook_name}-${count.index}"
@@ -40,5 +45,6 @@ resource "oci_datascience_notebook_session" "ods-notebook-session" {
   depends_on = [
     oci_identity_policy.ods-policy,
     oci_identity_policy.ods-root-policy,
+    time_sleep.wait_30_seconds
   ]
 }
